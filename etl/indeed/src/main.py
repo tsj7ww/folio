9c54156d5b5ff384655bb7dfc67e234709c711c8
Trevor Jordan <tsj7ww@gmail.com>
2020-09-17 21:37:58 -0400

    add generator for URL to minimize down time between scrapes

--- etl/indeed/src/main.py
+++ etl/indeed/src/main.py
@@ -2,8 +2,11 @@
 import json
 import os
 import io
+import sys
 import datetime
 import logging
+
+# etc
 import time
 import random
 import re
@@ -17,9 +20,7 @@ import requests
 import bs4
 
 def CONFIG(env):
-    """
-    Create ref dict
-    """
+    """"""
     if env=='dev':
         None
     elif env=='qa':
@@ -29,186 +30,202 @@ def CONFIG(env):
     else:
         raise Exception('Unknown environment.')
 
-    _name = 'Indeed'
-    _id = 'etl-indeed'
-    _dt = '%Y%m%d'
-    _dttm = '%Y%m%d_%H%M%S'
-    _now = datetime.datetime.now()
-    _region = 'us-east-1'
-    _rgn = _region.replace('-','')
+    cwd = os.getcwd()
+    now = datetime.datetime.now()
+    dt = '%Y%m%d'
+    dttm = '%Y%m%d_%H%M%S'
 
-    _cfg = {
-        'now': _now,
+    eid = 'tsj7ww'
+    proc = 'Indeed'
+    pid = 'etl-indeed'
+
+    region = 'us-east-1'
+    rgn = region.replace('-','')
+
+    cfg = {
+        'now': now,
+        'cwd': cwd,
+        'pid': pid,
+        'proc': proc,
+        'region': region,
+        'eid': eid,
         'arn': '{arn}:{partition}:{service}:{region}:{account-id}:{resource-id}',
         'indeed': {
             'url': 'https://www.indeed.com/jobs?start={NUM}&q={QRY}&l={LOC}',
             'replace': {' ':'%20',',':'%2C'}
         },
         'db': {
-            'url': 'https://dynamodb.{}.amazonaws.com'.format(_region),
-            'table': _id,
+            'url': 'https://dynamodb.{}.amazonaws.com'.format(region),
+            'table': pid
         },
         'email': {
             'Source': os.environ['SENDER'],
             'Destination': {'ToAddresses': os.environ['STAKEHOLDERS'].split(',')},
             'Message': {
                 'Subject': {'Data': 'Indeed Weekly Summary','Charset': 'UTF-8'},
-                'Body': {'Html': {'Data': None,'Charset': 'UTF-8'}},
+                'Body': {'Html': {'Data': None,'Charset': 'UTF-8'}}
             }
         },
         'alert': {
             'TopicArn': os.environ['SNS'],
             'Message': None,
-            'Subject': 'Process {}: ' + _name,
+            'Subject': 'Process {}: ' + proc
         }
     }
 
-    return _cfg
+    return cfg
 
 def LOGGER(env):
     """"""
+    f = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    l = logging.getLogger(__name__)
+
     if env=='dev':
-        _lvl = logging.DEBUG
+        lvl = logging.DEBUG
+        # log to stdout while in dev
+        s = None
+        h = logging.StreamHandler(sys.stdout)
     elif env=='qa':
-        _lvl = logging.WARNING
+        lvl = logging.WARNING
+        s = io.StringIO()
+        h = logging.StreamHandler(s)
     elif env=='prod':
-        _lvl = logging.ERROR
+        lvl = logging.ERROR
+        s = io.StringIO()
+        h = logging.StreamHandler(s)
     else:
         raise Exception('Unknown environment.')
 
-    _str = io.StringIO()
-    _handler = logging.StreamHandler(_str)
-    _log = logging.getLogger()
+    h.setFormatter(f)
+    h.setLevel(lvl)
+    l.setLevel(lvl)
+    l.addHandler(h)
 
-    _log.setLevel(_lvl)
-    _handler.setFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    _log.addHandler(_handler)
-
-    return _log,_str
+    return l,s
 
 def URL(url,titles,locations,replace,pages=1):
     """"""
-    _urls = []
+    logger = logging.getLogger(__name__)
+
     for t in titles:
         for l in locations:
-            _t = t
-            _l = l
+            _ = {'title': t,'location': l}
             for i,j in replace.items():
-                _t = _t.replace(i,j)
-                _l = l.replace(i,j)
+                t = t.replace(i,j)
+                l = l.replace(i,j)
             for p in range(pages):
-                _urls.append({
-                    'title': t,'location': l
-                    ,'url': url.format(NUM=10*p,QRY=_t,LOC=_l)
-                })
-    return _urls
-
-def EXTRACT(url):
-    """"""
-    time.sleep(random.randint(2,6))
-    return bs4.BeautifulSoup(requests.get(url).text,'html.parser')
+                _['url'] = url.format(NUM=10*p,QRY=t,LOC=l)
+                logger.info('Generating URL: {}'.format(_['url']))
+                yield _
 
 def CLEAN(item):
-    """quick function for cleaning scraped text data"""
-    _rm = re.compile('[^A-Za-z0-9\&\.\s]+')
-    _spc = re.compile('\s{2,}')
+    """"""
+    logger = logging.getLogger(__name__)
 
-    return re.sub(r'\s([."](?:\s|$))', r'\1',_spc.sub(' ',_rm.sub('',item))).strip().upper()
+    rm = re.compile('[^A-Za-z0-9\&\.\s]+')
+    spc = re.compile('\s{2,}')
+    base = re.compile(r'\s([."])(?:\s|$)')
 
-def TRANSFORM(soup):
-    """
-    in: soup obj
-    out: cleaned text
-    """
-    _url = 'www.indeed.com'
-    _skip = ['jobOppId','url_post']
-    _data = []
+    return base.sub(r'\1',spc.sub(' ',rm.sub('',item))).strip().upper()
+
+def EXTRACT(soup):
+    """"""
+    logger = logging.getLogger(__name__)
+
+    url = 'www.indeed.com'
+    skip = ['indeed_id','urlpost']
+    data = []
 
     for row in soup.find_all(name='div',attrs={'class':'row'}):
-        _post = {'jobOppId':str(uuid.uuid4())}
-        try:
-            _post['title'] = row.find(name='a',attrs={'data-tn-element':'jobTitle'}).text
-        except:
-            _post['title'] = None
-        try:
-            _post['company'] = row.find(name='a',attrs={'data-tn-element':'companyName'}).text
-        except:
-            _post['company'] = None
-        try:
-            _post['rating'] = row.find(name='span',attrs={'class':'ratingsContent'}).text
-        except:
-            _post['rating'] = None
-        try:
-            _post['city'] = row.find('div', attrs={'class': 'recJobLoc'})['data-rc-loc'].split(',')[0]
-        except:
-            _post['city'] = None
-        try:
-            _post['state'] = row.find('div', attrs={'class': 'recJobLoc'})['data-rc-loc'].split(',')[1]
-        except:
-            _post['state'] = None
-        try:
-            _post['salary'] = row.find('nobr').text
-        except:
-            _post['salary'] = None
-        try:
-            _post['summary'] = ' '.join([i.text for i in row.find('div',attrs={'class','summary'}).find_all('li')])
-        except:
-            _post['summary'] = None
-    #     try:
-    #         _post['duties'] = None
-    #     except:
-    #         _post['duties'] = None
-    #     try:
-    #         _post['skills'] = None
-    #     except:
-    #         _post['skills'] = None
-        try:
-            _post['url_post'] = _url+row.find(name='a',attrs={'data-tn-element':'jobTitle'})['href']
-        except:
-            _post['url_post'] = None
-    #     try:
-    #         _post['url_app'] = None
-    #     except:
-    #         _post['url_app'] = None
+        post = {'indeed_id':str(uuid.uuid4())}
 
-        for k,v in _post.items():
-            if (k not in _skip) & (v!=None):
-                _post[k] = CLEAN(v)
+        try:
+            post['title'] = row.find(name='a',attrs={'data-tn-element':'jobTitle'}).text
+        except:
+            post['title'] = None
+        try:
+            post['company'] = row.find(name='a',attrs={'data-tn-element':'companyName'}).text
+        except:
+            post['company'] = None
+        try:
+            post['rating'] = row.find(name='span',attrs={'class':'ratingsContent'}).text
+        except:
+            post['rating'] = None
+        try:
+            post['city'] = row.find('div', attrs={'class': 'recJobLoc'})['data-rc-loc'].split(',')[0]
+        except:
+            post['city'] = None
+        try:
+            post['state'] = row.find('div', attrs={'class': 'recJobLoc'})['data-rc-loc'].split(',')[1]
+        except:
+            post['state'] = None
+        try:
+            post['salary'] = row.find('nobr').text
+        except:
+            post['salary'] = None
+        try:
+            post['summary'] = ' '.join([i.text for i in row.find('div',attrs={'class','summary'}).find_all('li')])
+        except:
+            post['summary'] = None
+    #     try:
+    #         post['duties'] = None
+    #     except:
+    #         post['duties'] = None
+    #     try:
+    #         post['skills'] = None
+    #     except:
+    #         post['skills'] = None
+        try:
+            post['url_post'] = url+row.find(name='a',attrs={'data-tn-element':'jobTitle'})['href']
+        except:
+            post['url_post'] = None
+    #     try:
+    #         post['url_app'] = None
+    #     except:
+    #         post['url_app'] = None
 
-        _data.append(_post)
+        for k,v in post.items():
+            if (k not in skip) & (v!=None):
+                post[k] = CLEAN(v)
 
-    return _data
+        data.append(post)
+
+    return data
 
 def LOAD(data,url,table):
     """
     """
-    _responses = []
-    _db = boto3.resource('dynamodb', endpoint_url=url)
-    _tbl = _db.Table(table)
-    with _tbl.batch_writer() as b:
-        for r in data:
-            _r = b.put_item(Item=r)
-            if _r:
-                _responses.append(_r)
-    return _responses
+    logger = logging.getLogger(__name__)
+
+    fails = []
+    db = boto3.resource('dynamodb', endpoint_url=url)
+    tbl = db.Table(table)
+    with tbl.batch_writer() as b:
+        for d in data:
+            r = b.put_item(Item=d)
+            if r:
+                fails.append(r)
+    return fails
 
 def BODY(data):
     """"""
-    _body = 'Here are today\'s job postings:<br><br>'
-    _template = '<li><a href=\'{URL}\'>{TITLE} at {COMPANY} in {CITY}, {STATE}</li>'
-    _groups = []
-    [_groups.append((i['q_title'],i['q_location'])) for i in data if (i['q_title'],i['q_location']) not in _groups]
-    for i in _groups:
-        _row = '<b>{TTL} in {LOC}</b><br><ul>'.format(TTL=i[0],LOC=i[1])
-        for j in data:
-            if i==(j['q_title'],j['q_location']):
-                _row+=_template.format(
-                    URL=j['url_post'],
-                    TITLE=j['title'],COMPANY=j['company'],
-                    CITY=j['city'],STATE=j['state']
+    logger = logging.getLogger(__name__)
+
+    body = 'Here are today\'s job postings:<br><br>'
+    template = '<li>{TITLE} at {COMPANY} in {CITY}, {STATE} - <a href=\'{URL}\'>Link</a></li>'
+    groups = set((i['q_title'],i['q_location']) for i in data)
+    for g in groups:
+        rows = '<b>{TTL} in {LOC}</b>:<br><ul>'.format(TTL=g[0],LOC=g[1])
+        for d in data:
+            if g==(d['q_title'],d['q_location']):
+                rows+=template.format(
+                    URL=d['url_post'],
+                    TITLE=d['title'],COMPANY=d['company'],
+                    CITY=d['city'],STATE=d['state']
                 )
-        _body+=(_row+'</ul>')
-    return _body
+        body+=(row+'</ul>')
+
+    return body
 
 def ALERT(publish,success,err=None):
     """"""
@@ -218,14 +235,13 @@ def ALERT(publish,success,err=None):
     else:
         publish['Subject'] = publish['Subject'].format('Failure')
         publish['Message'] = err
-    sns = boto3.client('sns')
-    return sns.publish(**publish)
+
+    return boto3.client('sns').publish(**publish)
 
 def EMAIL(body,package):
     """"""
     package['Message']['Body']['Html']['Data'] = body
-    ses = boto3.client('ses')
-    return ses.send_email(**package)
+    return boto3.client('ses').send_email(**package)
 
 # if __name__=="__main__":
 def HANDLER(event, context):
@@ -234,62 +250,84 @@ def HANDLER(event, context):
     #     with open(os.path.join(os.getcwd(),'metadata.json'),'r') as f:
     #         META = json.load(f)
 
-    START = datetime.datetime.now()
-    ENV = 'dev'
-    with open(os.path.join(os.getcwd(),'env/{}'.format(ENV)),'r') as f:
+    start = datetime.datetime.now()
+    env = 'dev'
+    with open(os.path.join(os.getcwd(),'env/{}'.format(env)),'r') as f:
         os.environ.update(**json.load(f))
-    LOG,FEED = LOGGER(ENV)
-    CFG = CONFIG(ENV)
-    RESPONSE = {}
+
+    LOG,FEED = LOGGER(env)
+    CFG = CONFIG(env)
 
     # temporarily use static values -> transition to using event
-    if ENV=='dev':
-        PARAMS = {
+    if env=='dev':
+        fails = None
+        params = {
             'titles': ['data scientist'],
             'locations': ['seattle, wa']
         }
-    elif ENV=='prod':
-        PARAMS = {
+    elif env=='prod':
+        params = {
             'titles': ['data scientist','business analyst','data analyst','data engineer'],
             'locations': ['seattle, wa','austin, tx','washington, dc','richmind, va','boston, ma']
         }
+    else:
+        raise Exception('Unknown environment')
 
 
     try:
-        URLS = []
+        URLS = None
         POSTS = []
 
-        LOG.info('setup complete - generate urls')
-        URLS = URL(**PARAMS,**CFG['indeed'])
+        LOG.info('Setup complete - generate urls.')
+        # use generator to minimize downtime between requests
+        urls = URL(**params,**CFG['indeed'])
 
-        for q in URLS:
+        for q in urls:
 
-            LOG.info('get requests')
-            q['soup'] = EXTRACT(q['url'])
+            begin = datetime.datetime.now()
+            data = None
+            _ = {'q_title':q['title'].upper(),'q_location':q['location'].upper()}
 
-            LOG.info('transform data')
-            for i in TRANSFORM(q['soup']):
-                POSTS.append({**{'q_title':q['title'].upper(),'q_location':q['location'].upper()},**i})
+            LOG.info('Make get request')
+            q['soup'] = bs4.BeautifulSoup(requests.get(q['url']).text,'html.parser')
 
-        LOG.info('load data to dynamo db')
-        response = LOAD(POSTS,**CFG['db'])
+            LOG.info('Transform soup HTML to data')
+            data = [{**_,**i} for i in EXTRACT(q['soup'])]
+            POSTS.append(data)
 
-        LOG.info('sending notifications')
-        EMAIL(BODY(POSTS),CFG['email'])
-        ALERT(CFG['alert'],success=True)
-        LOG.info('job complete')
+            # LOG.info('Loading {} rows into DynamoDB'.format(len(data)))
+            # fails = LOAD(data,**CFG['db'])
+
+            # sleep random intervals to not get caught scraping
+            browse = random.randint(2,6)
+            t = max((browse - (datetime.datetime.now() - begin).seconds),0)
+            LOG.info('Sleeping for {} seconds'.format(t))
+            time.sleep(t)
+
+        # LOG.info('Sending email')
+        # EMAIL(BODY(POSTS),CFG['email'])
+        #
+        # LOG.info('Publishing event')
+        # ALERT(CFG['alert'],success=True)
+
+        runtime = (datetime.datetime.now()-start).seconds
+        LOG.info('Job complete after {} seconds!'.format(runtime))
+
         return {
             'status': 'success',
-            'data': [{'info': '{} queries containing {} posts completed in {} seconds.'.format(len(URLS),len(POSTS),(datetime.datetime.now()-START).seconds)}]
+            'summary': '{} posts pulled in {} seconds.'.format(len(POSTS),runtime),
+            'data': POSTS,
+            'fails': fails
         }
 
     except Exception as e:
-        _err = str(e)
-        LOG.critical(_err)
-        ALERT(CFG['alert'],success=False,err=_err)
+        err = str(e)
+        LOG.critical('Fatal error: {}'.format(err))
+        # ALERT(CFG['alert'],success=False,err=err)
         return {
             'status': 'failure',
-            'error': _err
+            'error': err
         }
 
 # print(HANDLER(None,None)) # testing
+# PROD changes: uncomment AWS service code, return JSON response
