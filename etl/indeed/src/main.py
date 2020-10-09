7cd7e5a74cbf7654d73a3e97dd126e91b97800b6
Trevor Jordan <tsj7ww@gmail.com>
2020-10-09 12:47:44 -0400

    add concurrency using concurrent.futures module

--- etl/indeed/src/main.py
+++ etl/indeed/src/main.py
@@ -5,6 +5,7 @@ import io
 import sys
 import datetime
 import logging
+import concurrent.futures
 
 # etc
 import time
@@ -101,38 +102,39 @@ def LOGGER(env):
 
     return logger,feed
 
-def URL(url,titles,locations,replace,pages=1):
+def QUERY_GENERATOR(url,titles,locations,replace,pages=1):
     """"""
     logger = logging.getLogger(__name__)
 
-    for t in titles:
-        for l in locations:
-            _ = {'title': t,'location': l}
-            for i,j in replace.items():
-                t = t.replace(i,j)
-                l = l.replace(i,j)
-            for p in range(pages):
-                _['url'] = url.format(NUM=10*p,QRY=t,LOC=l)
-                logger.info('Generating URL: {}'.format(_['url']))
-                yield _
+    for title in titles:
+        for location in locations:
+            query = {'title': title,'location': location}
+            for char,replace in replace.items():
+                title = title.replace(char,replace)
+                location = location.replace(char,replace)
+            for page in range(pages):
+                query['url'] = url.format(NUM=10*page,QRY=title,LOC=location)
+                logger.info('Generating URL: {}'.format(query['url']))
+
+                yield query
 
 def CLEAN(item):
     """"""
     logger = logging.getLogger(__name__)
 
-    rm = re.compile('[^A-Za-z0-9\&\.\s]+')
-    spc = re.compile('\s{2,}')
     base = re.compile(r'\s([."])(?:\s|$)')
+    extra = re.compile('[^A-Za-z0-9\&\.\s]+')
+    spaces = re.compile('\s{2,}')
 
-    return base.sub(r'\1',spc.sub(' ',rm.sub('',item))).strip().upper()
+    return base.sub(r'\1',spaces.sub(' ',extra.sub('',item))).strip().upper()
 
 def EXTRACT(soup):
     """"""
     logger = logging.getLogger(__name__)
 
     url = 'www.indeed.com'
-    skip = ['indeed_id','urlpost']
-    data = []
+    skip = ['indeed_id','url_post']
+    posts = []
 
     for row in soup.find_all(name='div',attrs={'class':'row'}):
         post = {'indeed_id':str(uuid.uuid4())}
@@ -182,13 +184,13 @@ def EXTRACT(soup):
     #     except:
     #         post['url_app'] = None
 
-        for k,v in post.items():
-            if (k not in skip) & (v!=None):
-                post[k] = CLEAN(v)
+        for field,value in post.items():
+            if (field not in skip) & (value!=None):
+                post[field] = CLEAN(value)
 
-        data.append(post)
+        posts.append(post)
 
-    return data
+    return posts
 
 def LOAD(data,url,table):
     """
@@ -198,11 +200,11 @@ def LOAD(data,url,table):
     fails = []
     db = boto3.resource('dynamodb', endpoint_url=url)
     tbl = db.Table(table)
-    with tbl.batch_writer() as b:
-        for d in data:
-            r = b.put_item(Item=d)
-            if r:
-                fails.append(r)
+    with tbl.batch_writer() as batch:
+        for item in data:
+            response = batch.put_item(Item=item)
+            if response:
+                fails.append(response)
     return fails
 
 def BODY(data):
@@ -211,15 +213,15 @@ def BODY(data):
 
     body = 'Here are today\'s job postings:<br><br>'
     template = '<li>{TITLE} at {COMPANY} in {CITY}, {STATE} - <a href=\'{URL}\'>Link</a></li>'
-    groups = set((i['q_title'],i['q_location']) for i in data)
-    for g in groups:
-        rows = '<b>{TTL} in {LOC}</b>:<br><ul>'.format(TTL=g[0],LOC=g[1])
-        for d in data:
-            if g==(d['q_title'],d['q_location']):
+    groups = set((item['q_title'],item['q_location']) for item in data)
+    for group in groups:
+        rows = '<b>{TTL} in {LOC}</b>:<br><ul>'.format(TTL=group[0],LOC=group[1])
+        for item in data:
+            if group==(item['q_title'],item['q_location']):
                 rows+=template.format(
-                    URL=d['url_post'],
-                    TITLE=d['title'],COMPANY=d['company'],
-                    CITY=d['city'],STATE=d['state']
+                    URL=item['url_post'],
+                    TITLE=item['title'],COMPANY=item['company'],
+                    CITY=item['city'],STATE=item['state']
                 )
         body+=(row+'</ul>')
 
@@ -240,6 +242,44 @@ def EMAIL(body,package):
     """"""
     package['Message']['Body']['Html']['Data'] = body
     return boto3.client('ses').send_email(**package)
+
+def ETL(query):
+    """"""
+    logger = logging.getLogger(__name__)
+
+    begin = datetime.datetime.now()
+    # prep vars
+    query_data = {'q_title':query['title'].upper(),'q_location':query['location'].upper()}
+    posts = []
+    failed_query = None
+    failed_loads = []
+    # get data
+    logger.info('Make get request')
+    response = requests.get(query['url'])
+    if response.status_code == requests.codes.ok:
+        # create soup object
+        query['soup'] = bs4.BeautifulSoup(response.text,'html.parser')
+        # extract data
+        logger.info('Transform soup HTML to data')
+        posts = [{**query_data,**post} for post in EXTRACT(query['soup'])]
+        # load data
+        logger.info('Loading {} rows into DynamoDB'.format(len(posts)))
+        failed_loads = LOAD(posts,**CFG['db'])
+        # sleep random intervals to not get caught scraping
+        browse = random.randint(2,6)
+        sleep = max((browse - (datetime.datetime.now() - begin).seconds),0)
+        logger.info('Sleeping for {} seconds'.format(sleep))
+        time.sleep(sleep)
+    else:
+        logger.DEBUG('Failed query url: {}'.format(query['url']))
+        failed_query=query
+
+    return {
+        'posts': data,
+        'failed_queries': failed_query,
+        'failed_loads': failed_loads,
+    }
+
 
 # if __name__=="__main__":
 def HANDLER(event, context):
@@ -273,41 +313,70 @@ def HANDLER(event, context):
 
 
     try:
-        URLS = None
+        QUERIES = []
         POSTS = []
+        FAILED_QUERIES = []
+        FAILED_LOADS = []
 
         LOG.info('Setup complete - generate urls.')
         # use generator to minimize downtime between requests
-        urls = URL(**params,**CFG['indeed'])
+        QUERIES = QUERY_GENERATOR(**params,**CFG['indeed'])
 
-        for q in urls:
+        ##################################
+        ### v1 - synchonous processing ###
+        ##################################
+        # for query in QUERIES:
+        #     begin = datetime.datetime.now()
+        #     # prep vars
+        #     query_data = {'q_title':query['title'].upper(),'q_location':query['location'].upper()}
+        #     posts = []
+        #     failed_query = None
+        #     failed_loads = []
+        #     # get data
+        #     LOG.info('Make get request')
+        #     response = requests.get(query['url'])
+        #     if response.status_code == requests.codes.ok:
+        #         query['soup'] = bs4.BeautifulSoup(response.text,'html.parser')
+        #         # extract data
+        #         LOG.info('Transform soup HTML to data')
+        #         posts = [{**query_data,**i} for i in EXTRACT(query['soup'])]
+        #         # load data
+        #         LOG.info('Loading {} rows into DynamoDB'.format(len(posts)))
+        #         failed_loads = LOAD(posts,**CFG['db'])
+        #         # allocate data
+        #         POSTS.append(posts)
+        #         FAILED_LOADS+=failed_loads
+        #         # sleep random intervals to not get caught scraping
+        #         browse = random.randint(2,6)
+        #         sleep = max((browse - (datetime.datetime.now() - begin).seconds),0)
+        #         LOG.info('Sleeping for {} seconds'.format(sleep))
+        #         time.sleep(sleep)
+        #     else:
+        #         LOG.DEBUG('Failed query url: {}'.format(query['url']))
+        #         failed_query = query
+        #         FAILED_QUERIES.append(failed_query)
 
-            begin = datetime.datetime.now()
-            data = None
-            _ = {'q_title':q['title'].upper(),'q_location':q['location'].upper()}
+        ########################################################
+        ### v2 - asynchonous threading w/ concurrent futures ###
+        ########################################################
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = executor.map(ETL,queries)
+            for result in concurrent.futures.as_completed(results):
+                try:
+                    POSTS.append(result['posts'])
+                    FAILED_LOADS+=result['failed_loads']
+                    FAILED_QUERIES.append(result['failed_query'])
+                except:
+                    raise
 
-            LOG.info('Make get request')
-            q['soup'] = bs4.BeautifulSoup(requests.get(q['url']).text,'html.parser')
-
-            LOG.info('Transform soup HTML to data')
-            data = [{**_,**i} for i in EXTRACT(q['soup'])]
-            POSTS.append(data)
-
-            # LOG.info('Loading {} rows into DynamoDB'.format(len(data)))
-            # fails = LOAD(data,**CFG['db'])
-
-            # sleep random intervals to not get caught scraping
-            browse = random.randint(2,6)
-            t = max((browse - (datetime.datetime.now() - begin).seconds),0)
-            LOG.info('Sleeping for {} seconds'.format(t))
-            time.sleep(t)
-
-        # LOG.info('Sending email')
-        # EMAIL(BODY(POSTS),CFG['email'])
-        #
-        # LOG.info('Publishing event')
-        # ALERT(CFG['alert'],success=True)
-
+        # email summary
+        LOG.info('Sending email')
+        body = BODY(POSTS)
+        EMAIL(body,CFG['email'])
+        # alert
+        LOG.info('Publishing event')
+        ALERT(CFG['alert'],success=True)
+        # check runtime
         runtime = (datetime.datetime.now()-start).seconds
         LOG.info('Job complete after {} seconds!'.format(runtime))
 
